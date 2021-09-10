9c984d637d799a0a3f1e8bfcc2335485acc94b37
David Gaither <gaitherdb@gmail.com>
2021-09-10 18:44:17 -0400

    add modal if form isnt filled out

--- public/js/library.js
+++ public/js/library.js
@@ -16,8 +16,10 @@ window.addEventListener('DOMContentLoaded', function () {
         const platform = document.querySelector('#platform').value;
         var beaten = document.querySelector('#beaten').checked;
         var currently_playing = document.querySelector('#currently_playing').checked;
+        const errorModal = document.querySelector('#staticBackdrop');
         
-        if (name && platform) {
+
+        if (name && platform != "Platform") {
             // Send a POST request to the API endpoint
             const response = await fetch('/api/games', {
                 method: 'POST',
@@ -26,15 +28,19 @@ window.addEventListener('DOMContentLoaded', function () {
             });
 
             if (response.ok) {
-
                 // If successful, refresh the page
                 document.location.reload();
             } else {
                 alert(response.statusText);
             }
-
+        } else {
+            errorModal.style.display= "block";
+            const modalClose = document.querySelector('.btn-close');
+            modalClose.onclick = function () {
+                errorModal.style.display= "none";
+            }
         }
-    };
+    }
 
     document
         .querySelector('#addGameForm')
