34a8cbe93e830dd23b39262968a1fda628161ce5
David Gaither <gaitherdb@gmail.com>
2021-09-10 22:06:04 -0400

    began game form edit

--- public/js/library.js
+++ public/js/library.js
@@ -2,6 +2,31 @@ window.addEventListener('DOMContentLoaded', function () {
     const addGameBtn = document.querySelector('#addGame');
     const addGameForm = document.querySelector('#addGameForm');
     const cancelBtn = document.querySelector('#cancelGame');
+    const gameList = document.querySelector('#ownedGames');
+    const editGameForm = document.querySelectorAll('.editGameForm');
+    const userGames = document.querySelectorAll('.userGames');
+
+    gameList.addEventListener("click", function (event) {
+
+        var element = event.target;
+        var game_id = element.parentElement.id;
+
+        for (let i = 0; i < editGameForm.length; i++) {
+            if (Number(editGameForm[i].id) == `${game_id}`) {
+                if (editGameForm[i].style.display === "none") {
+                    editGameForm[i].style.display = "block";
+
+                    for (let i = 0; i < userGames.length; i++) {
+
+                        if (userGames[i].id == `${game_id}`) {
+                            userGames[i].style.display = "none";
+
+                        }
+                    }
+                }
+            }
+        }
+    })
 
     addGameBtn.addEventListener("click", function () {
         if (addGameForm.style.display === "none") {
@@ -9,15 +34,15 @@ window.addEventListener('DOMContentLoaded', function () {
             addGameBtn.style.display = "none";
         }
     })
-    
-        
-        cancelBtn.addEventListener("click", function () {
-            console.log("hi")
-            if (addGameForm.style.display === "block") {
-                addGameForm.style.display = "none";
-                addGameBtn.style.display = "block";
-            }
-        })
+
+
+    cancelBtn.addEventListener("click", function () {
+
+        if (addGameForm.style.display === "block") {
+            addGameForm.style.display = "none";
+            addGameBtn.style.display = "block";
+        }
+    })
 
     const postGameHandler = async (event) => {
         event.preventDefault();
@@ -27,7 +52,7 @@ window.addEventListener('DOMContentLoaded', function () {
         var beaten = document.querySelector('#beaten').checked;
         var currently_playing = document.querySelector('#currently_playing').checked;
         const errorModal = document.querySelector('#staticBackdrop');
-        
+
 
         if (name && platform != "Platform") {
             // Send a POST request to the API endpoint
@@ -44,10 +69,10 @@ window.addEventListener('DOMContentLoaded', function () {
                 alert(response.statusText);
             }
         } else {
-            errorModal.style.display= "block";
+            errorModal.style.display = "block";
             const modalClose = document.querySelector('.btn-close');
             modalClose.onclick = function () {
-                errorModal.style.display= "none";
+                errorModal.style.display = "none";
             }
         }
     }
