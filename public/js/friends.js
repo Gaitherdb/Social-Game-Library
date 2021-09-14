9e59a5ffd7408c2abc6e00b7dc54e819ae6d4f8f
Nick Leon <nicolas7@vt.edu>
2021-09-14 14:20:45 -0400

    can request friend by entering their user email

--- public/js/friends.js
+++ public/js/friends.js
@@ -1,6 +1,6 @@
 const mainDiv = document.getElementById("friendlist");
 
-const myfunc = () => {
+const printFriends = () => {
 fetch('/api/friends')
   .then(
     function(response) {
@@ -28,4 +28,65 @@ fetch('/api/friends')
     console.log('Fetch Error :-S', err);
   });
 }
-myfunc();
+printFriends();
+
+const getuserId = async (event) => {
+  event.preventDefault();
+  console.log('friend req btn test')
+  const inputEmail = document.querySelector('#exampleInputEmail1').value.trim();
+
+  fetch('/api/users/test/')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data)
+        data.forEach(el => {
+
+          console.log(el.email)
+          console.log(inputEmail)
+
+          if (el.email===inputEmail){
+            newFormHandler(el.id)
+          }
+        })
+      });
+    }
+  )
+}
+
+const newFormHandler = async (id) => {
+  
+  // const destination_user_id = document.querySelector('#exampleInputEmail1').value.trim();
+  console.log(id)
+
+  const destination_user_id = id
+
+  if (destination_user_id) {
+    const response = await fetch(`/api/friends/request/`, {
+      method: 'POST',
+      body: JSON.stringify({destination_user_id}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log(JSON.stringify({destination_user_id}))
+
+    if (response.ok) {
+      console.log(response)
+    } else {
+      alert('Failed to create friend request');
+    }
+  }
+};
+
+
+document
+    .querySelector('.new-request-form')
+    .addEventListener('submit', getuserId);
