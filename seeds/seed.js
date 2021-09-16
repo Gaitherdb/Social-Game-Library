ca3b5ce30df4894737b4b2b6d5d31a41840ae878
Nick Leon <nicolas7@vt.edu>
2021-09-16 17:24:13 -0400

    feed integrated

--- seeds/seed.js
+++ seeds/seed.js
@@ -10,20 +10,20 @@ const requestData = require('./requestData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  // const users = await User.bulkCreate(userData, {
-  //   individualHooks: true,
-  //   returning: true,
-  // });
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-  // const games = await Game.bulkCreate(gameData, {
-  //   individualHooks: true,
-  //   returning: true,
-  // });
+  const games = await Game.bulkCreate(gameData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-  // const ownership = await Ownership.bulkCreate(ownershipData, {
-  //   individualHooks: true,
-  //   returning: true,
-  // });
+  const ownership = await Ownership.bulkCreate(ownershipData, {
+    individualHooks: true,
+    returning: true,
+  });
 
   // const friendship = await Friendship.bulkCreate(friendshipData, {
   //   individualHooks: true,
